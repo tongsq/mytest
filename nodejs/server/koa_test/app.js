fb9ea281bf2ff8ac64eedaef3201e9cebe8c9885
tongsq <tongsq77@qq.com>
2017-05-22 09:25:03 +0800

    save changes

--- nodejs/server/koa_test/app.js
+++ nodejs/server/koa_test/app.js
@@ -3,9 +3,45 @@ const Koa = require('koa');
 const router = require('koa-router')();
 const app = new Koa();
 const bodyParser = require('koa-bodyparser');
+const nunjucks = require('nunjucks');
+
+function createEnv(path, opts){
+    var
+	autoescape = opts.autoescape && true,
+	noCache = opts.noCache || false,
+	watch = opts.watch || false,
+	throwOnUndefined = opts.throwOnUndefined || false,
+	env = new nunjucks.Environment(
+	    new nunjucks.FileSystemLoader('views', {
+	        noCache: noCache,
+	        watch: watch
+	    }),{
+		autoescape: autoescape,
+		throwOnUndefined: throwOnUndefined
+	});
+    if (opts.filters){
+	for (var f in opts.filters){
+	    env.addFilter(f, opts.filters[f]);
+	}
+    }
+    return env;
+}
+var env = createEnv('views', {
+	watch: true,
+	filters: {
+	    hex: function(n) {
+		return 'Ox' + n.toString(16);
+	    }
+	}
+});
+
+
 app.use(async (ctx, next)=>{
     let stime = new Date().getTime();
-	console.log(ctx.request.body);
+    ctx.render = function(view, model){
+	ctx.response.body = env.render(view, Object.assign({}, ctx.state || {}, model ||{}));	 
+	ctx.response.type = 'text/html';
+    } 
     await next();
     var time = new Date().getTime() - stime;
     console.log(`Process ${ctx.request.method} ${ctx.request.url}...${time}ms`);
@@ -37,5 +73,11 @@ app.use(bodyParser());
 const number = 100;
 //app.use(router.routes());
 app.use(controller());
+
+//404 not found
+app.use(async (ctx,next)=>{
+    ctx.response.status = 404;
+    ctx.response.body = `404 Not Found`;
+});
 app.listen(8080);
 console.log(`app start at port 8080...\n`);
--- nodejs/server/koa_test/controllers/hello.js
+++ nodejs/server/koa_test/controllers/hello.js
@@ -1,6 +1,7 @@
 var fn_hello = async (ctx, next)=>{
     var name = ctx.params.name;
     //ctx.response.body = `<h1>Hello, ${name}!</h1>`;
+    console.log('render in hello.js');
     ctx.render('hello.html', {name:name});
 };
 
