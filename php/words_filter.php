e0d0934efb15896034319f0094c27a56cf8465b8
tongsq <tongsq77@qq.com>
2017-08-04 15:05:32 +0800

    使用mb_*函数，解决不同字符集的问题

--- php/words_filter.php
+++ php/words_filter.php
@@ -1,5 +1,6 @@
 <?php
 //采用树结构过滤关键字
+define('ENCODING', 'UTF-8');
 $filter_words = array('fehe','fuck','fuce','fuc','你好');
 $tree = array();
 //生成过滤树
@@ -9,24 +10,22 @@ foreach($filter_words as $words){
 }
 var_dump($tree);
 $content = $_SERVER['argv'][1];
-$len = strlen($content);
+$len = mb_strlen($content, ENCODING);
 for ($i=0; $i<$len; $i++)
 {
     $j = $i;
     $tmp = $tree;
     while (true)
 	{
-		if (isset($tmp[$content[$j]]))
+		$current = mb_substr($content, $j, 1, ENCODING);
+		if (isset($tmp[$current]))
 		{
-			if ($tmp[$content[$j]] === true){
-				for (; $i<=$j; $i++)
-				{
-					$content[$i] = '*';
-				}
+			if ($tmp[$current] === true){
+				$content = mb_substr($content, 0, $i, ENCODING) . str_repeat('*',($j-$i+1)) . mb_substr($content, $j+1, NULL, ENCODING);
 				$i = $j;
 				break;
 			}
-			$tmp = $tmp[$content[$j]];
+			$tmp = $tmp[$current];
 			$j++;
 		}
 		else{
@@ -36,11 +35,11 @@ for ($i=0; $i<$len; $i++)
 }
 var_dump($content);
 function createNode($words){
-	if (strlen($words) == 1){
+	if (mb_strlen($words, ENCODING) == 1){
 		return array($words=>true);
 	}
 	else{
-		return array($words[0]=>createNode(substr($words, 1)));
+		return array(mb_substr($words, 0, 1, ENCODING)=>createNode(mb_substr($words, 1, NULL, ENCODING)));
 	}
 }
 function mergeNode($node, &$tree){
