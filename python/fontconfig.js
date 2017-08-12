208c05b2fd0f94d18c96c3b569346b0b41118460
tongsq <tongsq77@qq.com>
2017-08-12 11:08:55 +0800

    add iconfont tools python

---
+++ python/fontconfig.js
@@ -0,0 +1,4 @@
+
+var map = {"gitafull":"58882","less":"59045","moreunfold":"59046","more":"59047","refresh":"59050","icon-02":"58891","selected":"59053","success":"59057","jitaleqi":"58930","creditlevelfilling":"59190","creditlevel":"59189","leqi":"58966","back":"59031","x":"120","close":"59034","delete":"59037",};
+;module.exports = (name)=>String.fromCharCode(map[name]);
+;module.exports.map = map;
