c611c3bd64b741cbe1cfa84ed85b16667d05b629
tongsq <tongsq77@qq.com>
2018-01-12 09:09:43 +0800

    add class @prototype demo

---
+++ python/test/fibclass.py
@@ -0,0 +1,19 @@
+class Fib(object):
+	def __init__(self):
+		self.a, self.b = 0, 1
+
+	def __iter__(self):
+		return self
+
+	def __next__(self):
+		self.a, self.b = self.b, self.a + self.b
+		if self.a > 100000:
+			raise StopIteration()
+		return self.a
+if __name__ == '__main__':
+	fib = Fib()
+	for value in fib:
+		if (value<100):
+			print(value, end=',')
+		else:
+			break
