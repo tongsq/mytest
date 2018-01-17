a3802ec85dc5ddacafffcf6a7b1fc9f0ab4890ad
tongsq <tongsq77@qq.com>
2018-01-17 16:50:03 +0800

    add swoole timer demo

--- swoole/timer.php
+++ swoole/timer.php
@@ -9,3 +9,15 @@ $after = swoole_timer_after(3000, function(){
 	swoole_timer_clear($tick);
 });
 echo "after start\n";
+$str = "hellor world\n";
+$str2 = "hellor world\n";
+$arr = array('a','b','c');
+\Swoole\Timer::tick(2000, function() use (&$str, &$arr){
+	//global $str,$arr;
+	foreach($arr as $value){
+		echo "$value ,";
+	}
+	$arr = array(1,2,3);	
+	echo $str;
+	$str = "aaa\n";
+});
